97fb67fa3d5a02cb3f86160c6c55336d3e1a0b50
taiki tsunekawa <s1f101702431@iniad.org>
2019-02-15 16:58:06 +0900

    [update] add test

--- test.cpp
+++ test.cpp
@@ -1,39 +1,78 @@
 #include "py_string.hpp"
 #include <iostream>
 
-template<class T,class U>
-int equal(T a,U b)
+using std::cout;
+using std::endl;
+using std::cerr;
+
+template <class T, class U>
+int equal(T a, U b)
 {
-    if(a == b)
+    if (a == b)
     {
-    std::cout << "pass    : " << a << " == " << b << std::endl;
+        cout << "pass    : " << a << " == " << b << endl;
         return 0;
     }
-    std::cerr << "Invalid : " << a << " != " << b << std::endl;
+    cerr << "Invalid : " << a << " != " << b << endl;
     return -1;
 }
 
 int main(int argc, char const *argv[])
 {
+    //    cout << "multiplication" << endl;
     py::string mul = "str";
-    equal(mul*2,"strstr");
-    equal(mul.pyfind("t"),1);
-    equal(mul.at(-1),'r');
-    equal(mul[-1],'r');
-    equal(mul.capitalize(),"Str");
-    equal(mul.center(12,'w'),"wwwwstrwwwww");
+    equal(mul * 2, "strstr");
+    mul *= 2;
+    equal(mul,"strstr");
+
+    //    cout << "pyfind" << endl;
+    equal(mul.pyfind("t"), 1);
+    equal(mul.pyfind("t", 1), 1);
+    equal(mul.pyfind("t", 2), 4);
+    equal(mul.pyfind("t", 3), 4);
+    equal(mul.pyfind("t", 4), 4);
+    equal(mul.pyfind("t", -1), -1);
+    equal(mul.pyfind("t", -2), 4);
+    equal(mul.pyfind("t", -3), 4);
+    equal(mul.pyfind("t", -4), 4);
+
+    mul = "str";
+    //    cout << "array subscription" << endl;
+    equal(mul.at(-1), 'r');
+    equal(mul[-1], 'r');
+
+    //    cout << "capitalize" << endl;
+    equal(mul.capitalize(), "Str");
+
+    //    cout << "center" << endl;
+    equal(mul.center(12, 'w'), "wwwwstrwwwww");
+
+    //    cout << "count" << endl;
     py::string cnt = mul * 10;
-    equal(cnt.count("str"),10);
-    equal(cnt.count("str",3,-3),8);
-    equal(cnt.endswith("r"),true);
-    equal(cnt.endswith("st",0,-1),true);
-    equal(cnt.endswith("t"),false);
+    equal(cnt.count("str"), 10);
+    equal(cnt.count("str", 3, -3), 8);
+    equal(cnt.count("str", -10), 3);
+    equal(cnt.count("str", -11), 3);
+    equal(cnt.count("str", -12), 4);
+
+    //    cout << "endswish" << endl;
+    equal(cnt.endswith("r"), true);
+    equal(cnt.endswith("st", 0, -1), true);
+    equal(cnt.endswith("t"), false);
+
+    //    cout << "pyreplace" << endl;
     py::string rep = "abcdabcd";
-    equal(rep.pyreplace("ab","ABA"),"ABAcdABAcd");
-    equal((mul*2).pyrfind("st"),3);
-    equal(cnt.startswith("tr",1),true);
-    equal(cnt.startswith("str",3),true);
-    equal(cnt.startswith("str",1),false);
+    equal(rep.pyreplace("ab", "ABA"), "ABAcdABAcd");
+
+    //    cout << "pyrfined" << endl;
+    equal((mul * 2).pyrfind("st"), 3);
+
+    //    cout << "startswish" << endl;
+
+    equal(cnt.startswith("tr", 1), true);
+    equal(cnt.startswith("str", 3), true);
+    equal(cnt.startswith("str", 1), false);
 
     return 0;
 }
+
